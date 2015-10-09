4276bf0d224d5d996f555f91fd28227a2ec4eaaa
batu89 <batu.zaya.89@gmail.com>
2015-10-10 00:51:01 +0800

    dummy data

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,8 @@ router.get('/', function(req, res, next){
 	res.render('index', {title: 'Neo4j-Node-App'})
 })
 
+router.get('/api_doc', function(req, res, next){
+	res.render('api_doc')
+})
+
 module.exports = router
