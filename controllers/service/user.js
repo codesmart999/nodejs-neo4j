0bd06323f0e278646d0ba53cd0d2c21e816fd5f3
batu89 <batu.zaya.89@gmail.com>
2015-10-10 01:58:56 +0800

    delete method added

--- controllers/service/user.js
+++ controllers/service/user.js
@@ -63,7 +63,7 @@ router.delete('/:id', function(req, res){
 	})
 })
 
-router.get('/del/:id', function(req, res){
+router.post('/del/:id', function(req, res){
 	users.del(req, res, function(err, node){
 		if (err){
 			console.log(err);
