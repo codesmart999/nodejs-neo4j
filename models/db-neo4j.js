85dc9011bf9ed1734df2d30094fedd00b0640b77
batu89 <batu.zaya.89@gmail.com>
2015-10-10 01:49:02 +0800

    api demo fixed

--- models/db-neo4j.js
+++ models/db-neo4j.js
@@ -1,8 +1,8 @@
 //Require the Node4j module
 var neo4j = require('node-neo4j');
 
-url = process.env.GRAPHENEDB_URL
-//url = 'http://neo4j:123456789@localhost:7474';
+//url = process.env.GRAPHENEDB_URL
+url = 'http://neo4j:123456789@localhost:7474';
 
 //Create a db object. We will use this object to work on the DB.
 db = new neo4j(url);
