6abb986af12e27b11d40196ea047749b3c7edbe6
batu89 <batu.zaya.89@gmail.com>
2015-10-11 23:25:24 +0800

    CORS fixed

--- routes/service.js
+++ routes/service.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
+router.all('/*', function(req, res, next){
+	res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Cache-Control, Pragma, Origin, Authorization, Content-Type, X-Requested-With");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+    return next();
+});
 router.use('/customer', require('./../controllers/service/customer'))
 router.use('/module', require('./../controllers/service/module'))
 router.use('/user', require('./../controllers/service/user'))
