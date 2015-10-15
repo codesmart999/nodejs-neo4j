2ae31245dacb78e36f62de3268d0abf7bf3934d0
batu89 <batu.zaya.89@gmail.com>
2015-10-16 01:13:06 +0800

    first commit

--- controllers/service/login.js
+++ controllers/service/login.js
@@ -9,6 +9,23 @@ router.post('/', function(req, res){
 			
 			res.json({status: err, message: node});
 		}else{
+			users.getRelationships(req, res, node[0], function(err, result){
+				if (err){
+					console.log(err);
+					
+					res.json({status: err, message: result});
+				}else{
+					console.log("Relationships:", result);
+					var user = res.user;
+					user.module = [];
+					
+					for (var i=0; i<result.length; i++)
+						user.module[i] = result[i]._end;
+					
+					res.json({status: 0, node: user});
+				}
+				res.end();
+			});
 			res.json({status: 0, node: node[0]});
 		}
 	});
