237192d5ead501e1d0d7031e972fc49e848f155d
batu89 <batu.zaya.89@gmail.com>
2015-10-16 01:38:44 +0800

    login modified

--- controllers/service/login.js
+++ controllers/service/login.js
@@ -9,24 +9,28 @@ router.post('/', function(req, res){
 			
 			res.json({status: err, message: node});
 		}else{
-			users.getRelationships(req, res, node[0], function(err, result){
-				if (err){
-					console.log(err);
-					
-					res.json({status: err, message: result});
-				}else{
-					console.log("Relationships:", result);
-					var user = res.user;
-					user.module = [];
-					
-					for (var i=0; i<result.length; i++)
-						user.module[i] = result[i]._end;
-					
-					res.json({status: 0, node: user});
-				}
-				res.end();
-			});
-			res.json({status: 0, node: node[0]});
+			if (node[0].userName == "admin" || node[0].userID)
+				res.json({status: 0, node: node[0]});
+			else{
+				users.getRelationships(req, res, node[0], function(err, result){
+					if (err){
+						console.log(err);
+						
+						res.json({status: err, message: result});
+					}else{
+						console.log("Relationships:", result);
+						var user = res.user;
+						user.module = [];
+						
+						for (var i=0; i<result.length; i++)
+							user.module[i] = result[i]._end;
+						
+						res.json({status: 0, node: user});
+					}
+					res.end();
+				});
+				res.json({status: 0, node: node[0]});
+			}
 		}
 	});
 })
