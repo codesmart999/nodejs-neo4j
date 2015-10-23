de720235c11717996a8b34596bdfed3e8fa10a57
batu89 <batu.zaya.89@gmail.com>
2015-10-23 21:17:02 +0800

    site module

--- models/zones.js
+++ models/zones.js
@@ -6,12 +6,12 @@ exports.all = function(req, res, cb){
 	var query = "";
 	if (req.params && req.params.customerID){
 		console.log("Trying to get Zones of Customer:" + req.params.customerID);
-		query = "MATCH (customer:User {userID:'" + req.params.customerID + "'})-[r1]-(region:Region)-[r2]-(zone:Zone)"
-			+ " RETURN zone.name, zone.manager, zone.email, zone.phone, zone.zipcode, zone.state, zone.country, zone.fax";
+		query = "MATCH (customer:User {userID:'" + req.params.customerID + "'})-[r1]-(region:Region)-[r2]-(site:Site)-[r3]-(zone:Zone)"
+			+ " RETURN zone.name, zone.zoneID";
 	}else{
 		console.log("Trying to get all Zones");
 		query = "MATCH (zone:Zone)"
-		+ " RETURN zone.name, zone.manager, zone.email, zone.phone, zone.zipcode, zone.state, zone.country, zone.fax";
+		+ " RETURN zone.name, zone.zoneID";
 	}
 
 	db.cypherQuery(query, function(err, node){
@@ -22,13 +22,7 @@ exports.all = function(req, res, cb){
 			for (var i=0; i<node.data.length; i++){
 				var item = {
 						name: node.data[i][0],
-						manager: node.data[i][1],
-						email: node.data[i][2],
-						phone: node.data[i][3],
-						zipcode: node.data[i][4],
-						state: node.data[i][5],
-						country: node.data[i][6],
-						fax: node.data[i][7]
+						zoneID: node.data[i][1]
 				};
 				result[result.length] = item;
 			}
@@ -54,17 +48,8 @@ exports.add = function(req, res, cb){
 	db.insertNode({
 		zoneID: _uuid,
 		name: req.body.name,
-		regionID: req.body.regionID,
+		siteID: req.body.siteID,
 		zonetypeID: req.body.zonetypeID,
-		manager: req.body.manager,
-		email: req.body.email,
-		phone: req.body.phone,
-		address1: req.body.address1,
-		address2: req.body.address2,
-		zipcode: req.body.zipcode,
-		state: req.body.state,
-		country: req.body.country,
-		fax: req.body.fax,
 	}, 'Zone', function(err, node){
 		if (err)
 			return cb("401", "Zone Name already exists!");
@@ -81,28 +66,10 @@ exports.edit = function(req, res, cb){
 	
 	if (req.body.name)
 		data.name = req.body.name;
-	if (req.body.manager)
-		data.manager = req.body.manager;
-	if (req.body.regionID)
-		data.regionID = req.body.regionID;
+	if (req.body.siteID)
+		data.siteID = req.body.siteID;
 	if (req.body.zonetypeID)
 		data.zonetypeID = req.body.zonetypeID;
-	if (req.body.email)
-		data.email = req.body.email;
-	if (req.body.phone)
-		data.phone = req.body.phone;
-	if (req.body.address1)
-		data.address1 = req.body.address1;
-	if (req.body.address2)
-		data.address2 = req.body.address2;
-	if (req.body.zipcode)
-		data.zipcode = req.body.zipcode;
-	if (req.body.state)
-		data.state = req.body.state;
-	if (req.body.country)
-		data.country = req.body.country;
-	if (req.body.fax)
-		data.fax = req.body.fax;
 
 	console.log("Trying to edit Zone:" + req.params.uuid, data);
 	db.updateNodesWithLabelsAndProperties('Zone', {zoneID:req.params.uuid}, data, cb);
@@ -134,13 +101,13 @@ exports.del = function(req, res, cb){
  * @zone: Newly Inserted Zone
  * @cb: callback function
  */
-exports.addRelationshipBetweenRegion = function(req, res, zone, cb){
-	console.log("Trying to create relationships FROM Region:", req.body.regionID);
+exports.addRelationshipBetweenSite = function(req, res, zone, cb){
+	console.log("Trying to create relationships FROM Site:", req.body.siteID);
 	console.log("Trying to create relationships TO Zone:", zone.zoneID);
 
-	var query = "MATCH (region:Region {regionID:'" + req.body.regionID + "'}),"
+	var query = "MATCH (site:Site {siteID:'" + req.body.siteID + "'}),"
 		+ "(zone:Zone {zoneID:'" + zone.zoneID + "'})"
-		+ " CREATE (region)-[r:Region_Zone]->(zone) RETURN r";
+		+ " CREATE (site)-[r:Site_Zone]->(zone) RETURN r";
 
 	db.cypherQuery(query, cb);
 }
