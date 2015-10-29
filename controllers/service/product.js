55aa87c1b68389152fec0870cd13a6a8ccbddfd2
anthony89 <anthony.kim.89@hotmail.com>
2015-10-29 11:05:41 -0700

    gun added

--- controllers/service/product.js
+++ controllers/service/product.js
@@ -79,40 +79,9 @@ router.post('/add', function(req, res){
 })
 
 router.post('/import', function(req, res){
-	var func_add_product = function(callback){
-		products.add(req, res, callback);
- 	};
- 	var func_add_relationship_customer = function(product, callback){
- 		res.product = product;
- 		products.addRelationshipBetweenCustomer(req, res, product, callback);
- 	}
- 	var func_add_relationship_producttype = function(result, callback){
- 		products.addRelationshipBetweenProducttype(req, res, res.product, callback);
- 	}
- 	var func_add_relationship_department = function(result, callback){
- 		products.addRelationshipBetweenDepartment(req, res, res.product, callback);
- 	}
-	
- 	var call_stack = [func_add_product,
- 	                  func_add_relationship_customer,
- 	                  func_add_relationship_producttype,
- 	                  func_add_relationship_department];
-	
- 	async.waterfall(
-			call_stack,
-			
-			//if succeeds, result will hold information of the relationship.
-			function(err, result){
-				if (err){
-					console.log(err);
-					
-					res.json({status: err, message: result});
-				}else{
-					res.json({status: 0});
-				}
-				res.end();
-			}
-	);
+	var product_index = -1;
+	console.log(req.body);
+	res.end();
 })
 
 router.post('/edit/:uuid', function(req, res){
